52d3e3b91354784cbd9e9c0bd1a6423115118ddc
张博 <zhangbo02@finupgroup.com>
2019-08-09 10:16:46 +0800

    求众数投票法

--- src/main/java/leetcode/array/MajorityElement.java
+++ src/main/java/leetcode/array/MajorityElement.java
@@ -43,8 +43,8 @@ public class MajorityElement {
      * 思路：
      * 1将数组无限划分，当只有一个元素时候 它就只子数组的众数
      * 2回溯当两个子数组
-     *  当两个子数组中的众数相同 那么他就是众数
-     *  不同时候 遍历数组 计算这个两个众数谁出现的次数多谁就是众数
+     * 当两个子数组中的众数相同 那么他就是众数
+     * 不同时候 遍历数组 计算这个两个众数谁出现的次数多谁就是众数
      *
      * @param nums
      * @return
@@ -82,11 +82,29 @@ public class MajorityElement {
         return leftCount > rightCount ? left : right;
     }
 
+    /**
+     * 众数 投票法
+     *
+     * @param nums
+     * @return
+     */
+    public int majorityElement3(int[] nums) {
+        int result = 0;
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (0 == count) {
+                result = nums[i];
+            }
+            count += result == nums[i] ? 1 : -1;
+        }
+        return result;
+    }
+
 
     public static void main(String[] args) {
         int[] ints = {8, 8, 7, 7, 7};
         MajorityElement majorityElement = new MajorityElement();
-        int i = majorityElement.majorityElement2(ints);
+        int i = majorityElement.majorityElement3(ints);
         System.out.println(i);
 
     }
